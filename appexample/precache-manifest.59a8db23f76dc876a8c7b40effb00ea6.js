75d3b67ee75794a36429d2b22652bc4db3007ad4
Matt Kellock <matt.kellock@iress.com>
2019-11-12 11:16:11 +1100

    Adding initial draft of application

---
+++ appexample/precache-manifest.59a8db23f76dc876a8c7b40effb00ea6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ae2ffa694dd2ec252074207e3f7520a",
+    "url": "/appexample/index.html"
+  },
+  {
+    "revision": "62a150aa684a67bd761c",
+    "url": "/appexample/static/css/main.fc772e40.chunk.css"
+  },
+  {
+    "revision": "fa8773abded382dcfa6b",
+    "url": "/appexample/static/js/2.543bafd2.chunk.js"
+  },
+  {
+    "revision": "62a150aa684a67bd761c",
+    "url": "/appexample/static/js/main.c9bd0f1d.chunk.js"
+  },
+  {
+    "revision": "816e19b66f56a5d0de7d",
+    "url": "/appexample/static/js/runtime-main.cb5b2f90.js"
+  },
+  {
+    "revision": "b3902ecc5490eb2d11d3e29cdc942a78",
+    "url": "/appexample/static/media/iPhone.b3902ecc.png"
+  },
+  {
+    "revision": "62b443e166dd3241e336d22c5a567f19",
+    "url": "/appexample/static/media/tram.62b443e1.png"
+  },
+  {
+    "revision": "111d9a99f7b842b9f70df115835d499e",
+    "url": "/appexample/static/media/trammask.111d9a99.png"
+  }
+]);
